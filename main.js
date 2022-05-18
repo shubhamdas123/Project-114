50cc1e8c0c429d63e0d6f4d9d255e45227eabaeb
shubhamdas123 <87475413+shubhamdas123@users.noreply.github.com>
2022-05-18 16:37:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(350, 300);
+    canvas.center();
+}
+
+function draw() {}
+
+function takeSnapshot() {
+    save('myFilterImage.png');
+}
